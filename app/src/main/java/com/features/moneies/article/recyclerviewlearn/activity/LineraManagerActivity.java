35a6a0b888500fa97cbbfe45f9b9372ca49392a0
Jetictors <zhengxcfutures@gmail.com>
2017-07-28 02:28:09 +0800

    ...

---
+++ app/src/main/java/com/features/moneies/article/recyclerviewlearn/activity/LineraManagerActivity.java
@@ -0,0 +1,11 @@
+package com.features.moneies.article.recyclerviewlearn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by loser's computer on 2017/7/28.
+ */
+
+public class LineraManagerActivity extends AppCompatActivity {
+
+}
